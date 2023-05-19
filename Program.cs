e653075b53e66d74e3092a1f30e081d76a02ca91
Tneswick Laptop <tneswick2@verizon.net>
2023-05-19 09:09:12 -0700

    initial commit from laptop

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> employees = new List<string>() { "adam", "amy" };
+
+            employees.Add("barbara");
+            employees.Add("billy");
+
+            Console.WriteLine("My employees include "  );
+            for (int i = 0; i < employees[].Length; i++)
+            {
+                
+            }
+        }
+    }
+}
+
